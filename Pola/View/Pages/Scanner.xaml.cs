e0c17bfe1879eb9a05319f4ae5f869956b6096d5
Łukasz Rejman <luke.rayman@hotmail.com>
2015-11-14 13:42:41 +0100

    Added EAN-8 to barcode validation

--- Pola/View/Pages/Scanner.xaml.cs
+++ Pola/View/Pages/Scanner.xaml.cs
@@ -319,7 +319,7 @@ namespace Pola.View.Pages
             });
         }
 
-        private static Regex eanRegex = new System.Text.RegularExpressions.Regex("^(\\d{12,13})$");
+        private static Regex eanRegex = new System.Text.RegularExpressions.Regex("^(\\d{8}|\\d{12,14})$");
         public static bool IsValidEan(string code) 
         {
             if (!(eanRegex.IsMatch(code))) return false; // Check if all digits and with 8, 12, 13 or 14 digits.
